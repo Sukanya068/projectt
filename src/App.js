4f71cd74eae7d924b80beb17c0c66b0551810f39
sukanya068 <ausukanya@hotmail.com>
2020-04-10 20:45:28 +0700

    สร้างlist

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
+//import './App.css';
 
 function App() {
+const [tasks, setTasks] = useState([
+  { id: 1, name: "do"},
+  {  id: 2, name: "doo"}
+])
+
+  const renderTask = () => {
+    return tasks.map((task, index) => {
+      
+      return (
+        <li key={index}> 
+        {' '}
+        {task.id} : {task.name} {' '}
+        </li>
+      )
+      })
+      
+  }
   return (
-    <div>Today
+    <div>
+      <h1>Today</h1>
+
+    <ul>{renderTask()}</ul>
 
     </div>
   );
