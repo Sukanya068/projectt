a1b575426ed24fc84f96d92254cfae7fc66054b1
sukanya068 <ausukanya@hotmail.com>
2020-04-15 16:34:32 +0700

    firebase

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyBgoBK1H59ID_rv9oZTJgjIRuifCGR-B14",
+    authDomain: "project2-ea05a.firebaseapp.com",
+    databaseURL: "https://project2-ea05a.firebaseio.com",
+    projectId: "project2-ea05a",
+    storageBucket: "project2-ea05a.appspot.com",
+    messagingSenderId: "284353088280",
+    appId: "1:284353088280:web:904acde2384801a2e66c6d",
+    measurementId: "G-CEXD1RNFY1"
+  };
+
+  export default config;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import config from './config'
+import firebase from 'firebase/app';
+import 'firebase/firestore'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if( firebase.apps.length === 0)
+    firebase.initializeApp(config);
+export const firestore = firebase.firestore()
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
